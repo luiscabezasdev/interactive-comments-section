d60b96de06aaefc853b4b07b4e362e6278584064
luiscabezasdev <luiscabezas25@hotmail.com>
2022-01-08 14:59:27 -0500

    Learning my second commit

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Hola Mundo
           </a>
           {' | '}
           <a
